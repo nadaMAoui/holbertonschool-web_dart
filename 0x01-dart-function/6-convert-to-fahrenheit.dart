77eff5395617b464c08112353924842b26d231f1
nadaMAoui <maaouinada747@gmail.com>
2024-07-19 21:49:08 +0200

    task 6

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,6 +1,8 @@
 List<double> convertToF(List<double> temperaturesInC) {
-  List<double> temperaturesInF = temperaturesInC.map((temperatureInC) {
-    return double.parse((temperatureInC * 9 / 5 + 32).toStringAsFixed(2));
-  }).toList();
-  return temperaturesInF;
+    List<double> temperaturesInF = [];
+    for (double tempC in temperaturesInC) {
+        double tempF = tempC * 9 / 5 + 32;
+        temperaturesInF.add(double.parse(tempF.toStringAsFixed(2)));
+    }
+    return temperaturesInF;
 }
