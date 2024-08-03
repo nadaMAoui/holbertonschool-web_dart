c7ce6b03ca0e2bc80a6a127e709f49ec9d930d7b
nadaMAoui <maaouinada747@gmail.com>
2024-08-03 05:29:20 +0200

    fixing

--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,33 +1,26 @@
-import 'dart:core';
-
 class Password {
-  String _password;
+  String? _password = "";
 
-  Password({required String password}) : _password = password;
+  String get password => _password!;
 
-  String get password => _password;
-
-  set password(String value) {
-    _password = value;
-  }
-
-  void updatePassword(String newPassword) {
-    _password = newPassword;
-  }
+  set password(String password) => _password = password;
+  Password({password}) : _password = password;
 
   bool isValid() {
-    RegExp hasUpper = RegExp(r'[A-Z]');
-    RegExp hasLower = RegExp(r'[a-z]');
-    RegExp hasDigit = RegExp(r'\d');
-    return _password.length >= 8 &&
-           _password.length <= 16 &&
-           hasUpper.hasMatch(_password) &&
-           hasLower.hasMatch(_password) &&
-           hasDigit.hasMatch(_password);
+    if ((this._password?.length ?? 0) > 6 &&
+        (this._password?.length ?? 0) < 18) {
+      if (this._password!.contains(RegExp(r"(?=.*[a-z])(?=.*[A-Z])\w+"))) {
+        if (this._password!.contains(RegExp(r'\d'))) {
+
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   @override
   String toString() {
-    return 'Your Password is: $_password';
+    return "Your Password is: ${this._password}";
   }
 }
